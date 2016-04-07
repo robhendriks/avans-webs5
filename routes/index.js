62faad17be78b5c1d0b8ae122bca3c6d261cd6e2
robhendriks <cyanidenl@icloud.com>
2016-04-08 01:32:59 +0200

    hierrr.. suk a ****

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.route('/')
     res.render('pages/index', { title: 'Dashboard', user: req.user });
   });
 
+router.route('/profile')
+  .get(auth('user'), function(req, res) {
+    res.render('pages/profile', { title: 'Profile', user: req.user });
+  });
+
 router.route('/docs')
   .get(auth('user'), function(req, res) {
     res.render('pages/docs', { title: 'Documentation', user: req.user, resources: resources });
